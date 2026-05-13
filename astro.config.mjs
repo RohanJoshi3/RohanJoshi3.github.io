decb3850b1234dbed177d9adf36a60b7c7c09edb
Rohan Joshi <rohanjoshi@Rohans-MacBook-Pro-5.local>
2026-05-13 15:40:00 -0400

    Redesign homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-export default {
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+export default defineConfig({
   site: 'https://rjoshi4464-rgb.github.io',
   base: '/rohanjoshi.github.io',
-};
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
